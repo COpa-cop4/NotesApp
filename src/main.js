a84f5a6395c6fdfbc610fab57f77ca0343733229
COpa-cop4 <atirrey392@gmail.com>
2025-02-10 17:52:06 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './script/customEl/note-data.js';
+import './script/customEl/note-item.js';
+import './script/data/dummy-notes.js';
+import './script/customEl/note-nav.js';
+import './script/customEl/note-foot.js';
+import './script/realtimeValidation.js';
